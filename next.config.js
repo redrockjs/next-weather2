29dfabf23cdabd7e706838b51998af445630934d
RedRock <smb.sochi@gmail.com>
2023-10-30 11:51:01 +0300

    fix: add eslint, prettier

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const path = require('path');
+const withImages = require('next-images');
+
+module.exports = withImages({
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/assets/scss')],
+    prependData: `@import "main.scss";`,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'https',
+        hostname: 'site.com',
+      },
+    ],
+  },
+  experimental: {
+    scrollRestoration: true,
+  },
+});
